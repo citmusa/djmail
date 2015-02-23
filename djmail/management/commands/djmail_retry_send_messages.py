278ff969f41c77b7af5499025943f643dc692410
David Fischer <david.fischer.ch@gmail.com>
2015-02-23 15:23:29 +0100

    Update tests

--- djmail/management/commands/djmail_retry_send_messages.py
+++ djmail/management/commands/djmail_retry_send_messages.py
@@ -1,7 +1,10 @@
 # -*- encoding: utf-8 -*-
 
+from __future__ import unicode_literals
+
 from django.core.management.base import NoArgsCommand
-from djmail import core
+
+from .. import core
 
 
 class Command(NoArgsCommand):
@@ -9,5 +12,4 @@ class Command(NoArgsCommand):
         core._send_pending_messages()
         core._mark_discarded_messages()
         core._retry_send_messages()
-
         return 0
--- djmail/template_mail.py
+++ djmail/template_mail.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 
-from __future__ import print_function, unicode_literals
+from __future__ import unicode_literals
 
 import logging
 from contextlib import contextmanager
